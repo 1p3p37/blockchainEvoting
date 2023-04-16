cff5cf80730efb9fff14707c3781aab3322d401a
1p3p37 <nejdan4ik86@gmail.com>
2023-04-16 16:03:30 +0300

    faet: Added verif. contr. using publish_source into deploy script

--- contracts/scripts/deploy.py
+++ contracts/scripts/deploy.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 import os
-from brownie import Vote, accounts, network
+from brownie import Voting, accounts, network
 from distutils.util import strtobool
 
 
 def main():
-    # dev = accounts.add(os.getenv('PRIVATE_KEY'))
-    dev = accounts[0]
+    dev = accounts.add(os.getenv('PRIVATE_KEY'))
+    # dev = accounts[0]
     print(network.show_active())
     # publish_source = True if os.getenv("ETHERSCAN_TOKEN") else False
-    from brownie.network.gas.strategies import GasNowScalingStrategy
-    gas_strategy = GasNowScalingStrategy("standard", "fast", 1.2)
-    deploy_args = [
-    ]
-    print('Deploy args:\n', deploy_args)
-    if input("Deploy Vote? y/[N]: ").lower() != "y":
-        return
+    # from brownie.network.gas.strategies import GasNowScalingStrategy
 
-    contract = Vote.deploy(
-       *deploy_args, publish_source=publish_source
-    )
-    print('Deployed contract', contract.address)
+    # gas_strategy = GasNowScalingStrategy("standard", "fast", 1.2)
+    # deploy_args = []
+    # print("Deploy args:\n", deploy_args)
+    # if input("Deploy Vote? y/[N]: ").lower() != "y":
+    #     return
+
+    contract = Voting.deploy({'from': dev}, publish_source=True)
+    print("Deployed contract", contract.address)
+
