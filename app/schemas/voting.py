5065e0679d2056f1974c0cc58cd25f08ae78fbc0
1p3p37 <nejdan4ik86@gmail.com>
2023-08-04 03:11:50 +0300

    feat: added aioredis cache support and changed voting api for that

--- app/schemas/voting.py
+++ app/schemas/voting.py
@@ -1,33 +1,28 @@
 from datetime import datetime
-from decimal import Decimal
-from typing import Optional
-
 from pydantic import BaseModel
-
-from app.utils import decimal_to_str
 
 
 class VoteInDB(BaseModel):
     id: int
     voting_id: int
     option_id: int
+    tx_hash: str | None
     voter_address: str
+    voted_for: str
     block_number: int
-    created_at: Optional[datetime]
+    created_at: datetime | None
+    is_revote: bool
 
     class Config:
         orm_mode = True
 
 
-# '''
-# id = Column(Integer, primary_key=True, index=True)
-# voting_id = Column(Integer, ForeignKey("voting.id"))
-# tx_hash = Column(String, unique=True, nullable=False, server_default=text("random()"))
-# voting = relationship("Voting", back_populates="votes")
-# option_id = Column(Integer, ForeignKey("option.id"))
-# option = relationship("Option")
-# voter_address = Column(String)
-# voted_for = Column(String)
-# is_revote = Column(Boolean, default=False)
-# block_number = Column(Integer)
-# '''
+class VotingInDB(BaseModel):
+    tx_hash: str | None
+    id: int
+    name: str
+    start_time: int
+    end_time: int
+
+    class Config:
+        orm_mode = True
